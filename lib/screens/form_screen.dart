1d1b86e206d7586b7cfdff294280a42de7bc3816
Vitória Chaves <vitoriagrier@gmail.com>
2023-09-07 17:51:52 -0300

    adiciona componente de form

---
+++ lib/screens/form_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FormScreen extends StatefulWidget {
+  @override
+  State<FormScreen> createState() => _FormScreenState();
+}
+
+class _FormScreenState extends State<FormScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
