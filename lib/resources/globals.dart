1121f6784c32083ba993d0fbf7d12598b7165783
Vitória Chaves <vitoriagrier@gmail.com>
2023-09-07 17:20:57 -0300

    remove tom pastel

--- lib/resources/globals.dart
+++ lib/resources/globals.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 const Color primario = Color.fromARGB(223, 27, 76, 120);
-const Color pastel = Color.fromARGB(223, 204, 228, 248);
 const Color secondary = Color.fromARGB(255, 54, 110, 175);
 const Color terciary = Color.fromARGB(255, 74, 197, 211);
 const Color quaternary = Colors.white;
